221288db44c594b7eca4fb46c1a696e7709c35c3
Katia <kushnir.ekaterina@gmail.com>
2023-06-13 12:56:01 +0200

    Add APIs

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+
+const urlDatabase = {};
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +20,37 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// API endpoint for URL shortening
+app.post('/api/shorturl', function(req, res) {
+  const longUrl = req.body.url;
+  const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+  if (!urlRegex.test(longUrl)) {
+    res.status(400).json({error: 'invalid url'});
+    return;
+  }
+  const shortUrl = Object.keys(urlDatabase).length + 1;
+  urlDatabase[shortUrl] = longUrl;
+
+  res.json(
+    {
+      original_url: longUrl, 
+      short_url: shortUrl
+    }
+  );
+});
+
+// Redirect to the original URL
+app.get('/api/shorturl/:shortUrl', function(req, res) {
+  const shortUrl = req.params.shortUrl;
+  const longUrl = urlDatabase[shortUrl];
+
+  if (longUrl) {
+    res.redirect(301, longUrl);
+  } else {
+    res.status(404).json({error: 'URL not found'});
+  }
 });
 
 app.listen(port, function() {
